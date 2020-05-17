945e1128e5223b3a82083e8cc62dde32a6f3ef3b
GeunYeong Cheon <24crazyoung@gmail.com>
2020-05-17 10:56:39 +0900

    [signal] Add simple websocket connection

--- signaling-server/src/index.ts
+++ signaling-server/src/index.ts
@@ -1,10 +1,29 @@
+/* eslint-disable no-console */
 import express from 'express';
+import WebSocket from 'ws';
 import PORT from './config/constants';
+
+
+const wss = new WebSocket.Server({
+  port: 4000,
+});
+
+wss.on('connection', (ws: WebSocket) => {
+  ws.on('message', (message: WebSocket.Data) => {
+    console.log('received: %s', message);
+  });
+
+  ws.send('Hello Client!');
+});
+
 
 const app = express();
 app.use(express.json());
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`server is listening on port ${PORT}`);
+});
+
+app.get('/', (req : express.Request, res : express.Response) => {
+  res.send('pong');
 });
