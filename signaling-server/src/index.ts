e21ac087c601237e210d5c948b94ee3de4f2886d
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-13 19:17:27 +0900

    [signal] Test logging request headers

--- signaling-server/src/index.ts
+++ signaling-server/src/index.ts
@@ -40,6 +40,10 @@ wss.on('connection', (ws: WebSocket, request: IncomingMessage) => {
   const ip2 = request.socket.remoteAddress;
   console.log('ip2 is ', ip2);
 
+  const { headers, rawHeaders } = request;
+  console.log('headers is ', headers);
+  console.log('rawHeaders is ', rawHeaders);
+
 
   ws.on('message', (message: WebSocket.Data) => {
     // [15:12 Sun 07 Jun 2020] string only for now
