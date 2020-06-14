59581293116e97c6c553efbc2a73c290984485aa
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-15 05:03:32 +0900

    [client] Construct basic app structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
 import './utils/localApi';
+import HeaderComponent from './components/HeaderComponent';
+import PeerTabComponent from './components/Message/PeerTabComponent';
+import MessageTableComponent from './components/Message/MessageTableComponent';
+import FooterComponent from './components/FooterComponent';
+import SendComponent from './components/Send/SendComponent';
+
+const Container = styled.div`
+  margin: 200px;
+  border: solid 2px black;
+  background: #ffff0030;
+`;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <HeaderComponent />
+          <SendComponent />
+          <PeerTabComponent />
+          <MessageTableComponent />
+          <FooterComponent />
+        </Container>
       </div>
     );
   }
