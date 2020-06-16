a1f80769bcf4b21549f10e90aa87055ed3bc33fe
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-16 09:44:58 +0900

    [client] Connect PeerTabComponent to store

--- client/src/components/Message/PeerTabComponent.js
+++ client/src/components/Message/PeerTabComponent.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import { connectToPeer } from '../../utils/localApi';
+import { updatePeerUUID } from '../../redux/action';
 
 
 const PeerTab = styled.div`
@@ -36,11 +38,23 @@ class PeerTabComponent extends Component {
     this.state = {
       mockPeers: ['Peer1', 'Peer2', 'Peer3'],
     };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(uuid) {
+    console.log('uuid is ', uuid);
+
+    this.props.dispatch(updatePeerUUID(uuid));
+    connectToPeer(uuid);
   }
 
   render() {
     const { mockPeers } = this.state;
     const { peers } = this.props;
+
+    console.log('this.props is ', this.props);
+    console.log('peers is ', peers);
 
     return (
       <Fragment>
@@ -60,7 +74,7 @@ class PeerTabComponent extends Component {
           {
             peers && peers.map(peer => {
               return (
-                <PeerTabButton>
+                <PeerTabButton onClick={ this.onClick.bind(this, peer) }>
                   { `#${peer}` }
                 </PeerTabButton>
               )
