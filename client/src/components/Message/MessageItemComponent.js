eea77080162a08e9db5d14890f6895cdd6f8e5dd
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-21 16:59:14 +0900

    [client] Implement copy text for text type message packet

--- client/src/components/Message/MessageItemComponent.js
+++ client/src/components/Message/MessageItemComponent.js
@@ -15,7 +15,51 @@ const MessageCell = styled.div`
   margin: 0 10px;
 `;
 
+const MessageButtonCell = styled.button`
+  border: solid 1px black;
+  margin: 0 10px;
+  outline: none;
+
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
+
 class MessageItemComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onDownloadOrCopy = this.onDownloadOrCopy.bind(this);
+  }
+
+  onDownloadOrCopy() {
+    const { messagePacket } = this.props;
+    const { source, destination, type, data } = messagePacket;
+
+    if (type === PEER_MESSAGE_TYPE.TEXT) {
+      const { message } = data;
+
+      navigator.clipboard.writeText(message).then(function() {
+        // TODO: notify user that copy to clip succeed
+      }, function() {
+        // TODO: notify user that copy to clip failed
+      });
+
+
+      return;
+    }
+
+    if (type === PEER_MESSAGE_TYPE.FILE) {
+
+      return;
+    }
+
+    if (type === PEER_MESSAGE_TYPE.ERROR) {
+      // TODO: ???? do same as text type?..
+    }
+  }
+
   renderContent(messageType, data) {
     if (messageType === PEER_MESSAGE_TYPE.TEXT) {
       return (
@@ -39,7 +83,7 @@ class MessageItemComponent extends Component {
       return;
     }
 
-    const { source, destination, type, data, size } = messagePacket;
+    const { source, destination, type, data } = messagePacket;
 
     return (
       <Fragment>
@@ -47,6 +91,15 @@ class MessageItemComponent extends Component {
         <MessageCell>{ `${ destination }` }</MessageCell>
         <MessageCell>{ `${ type }` }</MessageCell>
         { this.renderContent(type, data) }
+        <MessageButtonCell onClick={ this.onDownloadOrCopy }>
+          {
+            type === PEER_MESSAGE_TYPE.DOWNLOAD
+            ? 'Download'
+            : 'Copy'
+          }
+        </MessageButtonCell>
+        <MessageButtonCell>+</MessageButtonCell>
+        <MessageButtonCell>Delete</MessageButtonCell>
       </Fragment>
     );
   }
