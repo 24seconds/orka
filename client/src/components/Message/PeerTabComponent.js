919b75e1956737600ec1bd31844b2ba08ee1de36
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-22 01:24:24 +0900

    [client] Remove mockup peers

--- client/src/components/Message/PeerTabComponent.js
+++ client/src/components/Message/PeerTabComponent.js
@@ -3,10 +3,6 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { connectToPeer } from '../../utils/localApi';
 import { updatePeerUUID } from '../../redux/action';
-import StreamSaver from 'streamsaver';
-import { generateFingerPrint } from '../../utils/messagePacket';
-
-StreamSaver.mitm = `${ process.env.REACT_APP_MITM_URL }/mitm.html?version=2.0.0`;
 
 const PeerTab = styled.div`
   background: blue;
@@ -38,12 +34,7 @@ class PeerTabComponent extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      mockPeers: ['Peer1', 'Peer2', 'Peer3'],
-    };
-
     this.onClick = this.onClick.bind(this);
-    this.onTestPostMessage = this.onTestPostMessage.bind(this);
   }
 
   onClick(uuid) {
@@ -53,57 +44,7 @@ class PeerTabComponent extends Component {
     connectToPeer(uuid);
   }
 
-  async onTestPostMessage() {
-    const byte = new TextEncoder().encode("hahaha");
-
-    const options = {
-      pathname: generateFingerPrint(),
-      size: 1024 * byte.length,
-    };
-    const fileStream = StreamSaver.createWriteStream('test_streamsaver2.txt', options);
-    window.fileStream = fileStream;
-
-    const writer = fileStream.getWriter();
-    window.writer = writer;
-
-    window.onunload = () => window.writer.abort();
-
-    writer.write(byte)
-    let i = 1
-    const interval = setInterval(() => {
-      writer.write(byte)
-      i++
-      writer.write(byte)
-      i++
-      writer.write(byte)
-      i++
-      writer.write(byte)
-      i++
-
-      if (i >= 1024) {
-        writer.close()
-        clearInterval(interval)
-
-        const iframes = document.querySelectorAll('iframe');
-
-        console.log('iframes is ', iframes);
-
-        iframes.forEach(iframe => {
-          console.log('iframe is ', iframe);
-          console.log('iframe.src is ', iframe.src);
-
-          if (iframe.src.includes(options.pathname)) {
-            console.log('remove iframe: ', options.pathname);
-            iframe.remove();
-          }
-        })
-      }
-    }, 1);
-
-  }
-
   render() {
-    const { mockPeers } = this.state;
     const { peers } = this.props;
 
     console.log('this.props is ', this.props);
@@ -112,22 +53,15 @@ class PeerTabComponent extends Component {
     return (
       <Fragment>
         <PeerTab className='localdrop-peer-tab'>
-          <PeerTabButton>
+          <PeerTabButton key='ALL'>
             ALL
           </PeerTabButton>
           {
-            mockPeers.map(peer => {
-              return (
-                <PeerTabButton onClick={ this.onTestPostMessage }>
-                  { `#${peer}` }
-                </PeerTabButton>
-              )
-            })
-          }
-          {
             peers && peers.map(peer => {
               return (
-                <PeerTabButton onClick={ this.onClick.bind(this, peer) }>
+                <PeerTabButton
+                  key={ peer }
+                  onClick={ this.onClick.bind(this, peer) }>
                   { `#${peer}` }
                 </PeerTabButton>
               )
