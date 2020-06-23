d67e994cfb60f8943ed78c4b9e9d5337a25409af
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-24 03:12:30 +0900

    [client] Add MaterialTheme Oceanic Color System

--- client/src/constants/styleConstants.js
+++ client/src/constants/styleConstants.js
@@ -4,3 +4,32 @@ export const TabSmallWidth = 50;
 export const TabDefaultWidth = 100;
 export const TabContentWidth = 200;
 export const mobileWidth = '620px'
+
+export const MaterialThemeOceanic = {
+  'Background': '#263238',
+  'Foreground': '#B0BEC5',
+  'Text': '#607D8B',
+  'SelectionBackground': '#546E7A',
+  'SelectionForeground': '#FFFFFF',
+  'Buttons': '#2E3C43',
+  'SecondBackground': '#32424A',
+  'Disabled': '#415967',
+  'Contrast': '#1E272C',
+  'Active': '#314549',
+  'Border': '#2A373E',
+  'Highlight': '#425B67',
+  'Tree': '#546E7A',
+  'Notifications': '#1E272C',
+  'AccentColor': '#009688',
+  'ExcludedFilesColor': '#2E3C43',
+  'CommentsColor': '#546e7a',
+  'LinksColor': '#80cbc4',
+  'FunctionsColor': '#82aaff',
+  'KeywordsColor': '#c792ea',
+  'TagsColor': '#f07178',
+  'StringsColor': '#c3e88d',
+  'OperatorsColor': '#89ddff',
+  'AttributesColor': '#ffcb6b',
+  'NumbersColor': '#f78c6c',
+  'ParametersColor': '#f78c6c',
+};
