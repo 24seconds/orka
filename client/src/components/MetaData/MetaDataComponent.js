6bd8eaa3d2e15182e404d6740c5bcb9f0646d6fc
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-26 20:02:56 +0900

    [client] Add MetaDataComponent. I forgot

---
+++ client/src/components/MetaData/MetaDataComponent.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+import { MaterialThemeOceanic } from '../../constants/styleConstants';
+
+
+const MyUUID = styled.div`
+  display: flex;
+  height: 20px;
+  margin: 0 0 10px 10px;
+  align-items: center;
+  font-size: 14px;
+  color: ${ MaterialThemeOceanic.StringsColor };
+  background-color: ${ MaterialThemeOceanic.SecondBackground };
+
+  span {
+    margin-right: 8px;
+    color: ${ MaterialThemeOceanic.ParametersColor };
+  }
+`;
+
+class MetaDataComponent extends Component {
+  render() {
+    const { myUUID } = this.props
+
+    return (
+      <MyUUID>
+        <span>My uuid:</span>
+        { `${ myUUID ? ('#'+ myUUID) : '' }` }
+      </MyUUID>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  myUUID: state.myUUID,
+});
+export default connect(mapStateToProps)(MetaDataComponent);
