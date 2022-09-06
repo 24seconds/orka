e9ec9cc940a8cec262657dc30e9f9c66a6c97437
24seconds <34280965+24seconds@users.noreply.github.com>
2022-09-06 13:16:23 +0900

    write db schema (#30)

---
+++ client/src/database/schema.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS users (
+    id CHAR(16) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    profile INT8 NOT NULL
+);
+-- data: file type
+CREATE TABLE IF NOT EXISTS files (
+    id CHAR(16) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    size INT32 NOT NULL,
+    download_count INT16 NOT NULL,
+    hands_up BOOLEAN NOT NULL,
+    uploaded_by CHAR(16) NOT NULL,
+    uploaded_at TIMESTAMP NOT NULL
+);
+-- data: link type
+CREATE TABLE IF NOT EXISTS links (
+    id CHAR(16) PRIMARY KEY,
+    text TEXT NOT NULL,
+    view_count INT16 NOT NULL,
+    hands_up BOOLEAN NOT NULL,
+    uploaded_by CHAR(16) NOT NULL,
+    uploaded_at TIMESTAMP NOT NULL
+);
+CREATE TABLE IF NOT EXISTS comments (
+    id CHAR(16) PRIMARY KEY,
+    data_id CHAR(16) NOT NULL,
+    data_type VARCHAR(16) NOT NULL,
+    text TEXT NOT NULL,
+    sender_id CHAR(16) NOT NULL,
+    receiver_id CHAR(16) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+CREATE TABLE IF NOT EXISTS comment_metadata (
+    data_id CHAR(16) PRIMARY KEY,
+    last_read_comment_id CHAR(16)
+);
+CREATE TABLE IF NOT EXISTS notifications (
+    id CHAR(16) PRIMARY KEY,
+    text TEXT NOT NULL,
+    data_id CHAR(16) NOT NULL,
+    data_type VARCHAR(16) NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
